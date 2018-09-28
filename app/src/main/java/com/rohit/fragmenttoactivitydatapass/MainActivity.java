46ae1fd4c3510c23a6224cee545bca7e2a82173e
Rohit Kumar <rohit9075@gmail.com>
2018-09-28 09:23:30 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/fragmenttoactivitydatapass/MainActivity.java
@@ -0,0 +1,34 @@
+package com.rohit.fragmenttoactivitydatapass;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showFragment(); // show fragment method call
+
+    }
+
+
+
+    
+
+    private void showFragment() {
+        FirstFragment firstFragment = new FirstFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.add(R.id.main_container,firstFragment, "firstFragment");
+
+        transaction.commit();
+    }
+}
